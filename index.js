5a24cd03e2c428691afb4d78c0f75a03088067bb
Michael Dinh <mkdinh94@gmail.com>
2018-11-20 00:04:54 -0500

    add figlet

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
 
 const config = require("./config/config.json");
 const Prompt = require("./lib/Prompt");
+const figlet = require("figlet");
 
 const prompt = new Prompt();
 
-prompt.init(config);
+figlet("BC Zipper", (err, str) => {
+  if (err) throw err;
+
+  config.title = str;
+
+  prompt.init(config);
+});
